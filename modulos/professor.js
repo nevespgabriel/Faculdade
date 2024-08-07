1452028f729d3aead49752cf20ee64fe2fc14f6d
nevespgabriel <nevespgabriel@gmail.com>
2024-08-07 09:12:33 -0300

    feat: crud professor, sala, turno

---
+++ modulos/professor.js
@@ -0,0 +1,62 @@
+const prompt = require("prompt-sync")();
+const db = [];
+let proximoId = 1;
+
+const modelo = (id = proximoId) => {
+    const nome = prompt("Digite o nome: ").trim();
+    if(nome != ""){
+        console.log("Cadastro realizado com sucesso!");
+        return {
+            id, 
+            nome
+        }
+    }
+    console.log("ERRO! Dados inválidos")
+}
+
+const show = (id) => db.find((el) => el.id == id);
+
+const store = () => {
+    const el = modelo();
+    if(el){
+        proximoId++;
+        db.push(el);
+    }
+}
+
+const index = () => {
+    if(db.length == 0){
+        console.log("Nenhum cadastro realizado.");
+        return false;
+    }
+    db.forEach(aluno => console.log(aluno));
+    return true;
+}
+
+const update = () => {
+    if(index()){
+        const id = parseInt(prompt("Digite o id do elemento que deseja alterar: ").trim());
+        const indice = show(id);
+        if(indice){
+            db[indice] = modelo(id);
+        }
+    }
+}
+
+const destroy = () => {
+    if(index()){
+        const id = parseInt(prompt("Digite o id do elemento que deseja alterar: ").trim());
+        const indice = show(id);
+        if(indice){
+            db.splice(indice, 1);
+            console.log("Elemento excluído com sucesso.");
+        }
+    }
+}
+
+module.exports = {
+    store,
+    index,
+    update,
+    destroy
+}
---
+++ modulos/turno.js
@@ -0,0 +1,66 @@
+const prompt = require("prompt-sync")();
+const db = [];
+let proximoId = 1;
+
+const modelo = (id = proximoId) => {
+    const nome = prompt("Digite o nome: ").trim();
+    const inicio = prompt("Início: ").trim();
+    const termino = prompt("Término: ").trim();
+    if(nome != "" && !isNaN(inicio) && !isNaN(termino)){
+        console.log("Cadastro realizado com sucesso!");
+        return {
+            id, 
+            nome,
+            inicio,
+            termino
+        }
+    }
+    console.log("ERRO! Dados inválidos")
+}
+
+const show = (id) => db.find((el) => el.id == id);
+
+const store = () => {
+    const el = modelo();
+    if(el){
+        proximoId++;
+        db.push(el);
+    }
+}
+
+const index = () => {
+    if(db.length == 0){
+        console.log("Nenhum cadastro realizado.");
+        return false;
+    }
+    db.forEach(aluno => console.log(aluno));
+    return true;
+}
+
+const update = () => {
+    if(index()){
+        const id = parseInt(prompt("Digite o id do elemento que deseja alterar: ").trim());
+        const indice = show(id);
+        if(indice){
+            db[indice] = modelo(id);
+        }
+    }
+}
+
+const destroy = () => {
+    if(index()){
+        const id = parseInt(prompt("Digite o id do elemento que deseja alterar: ").trim());
+        const indice = show(id);
+        if(indice){
+            db.splice(indice, 1);
+            console.log("Elemento excluído com sucesso.");
+        }
+    }
+}
+
+module.exports = {
+    store,
+    index,
+    update,
+    destroy
+}
